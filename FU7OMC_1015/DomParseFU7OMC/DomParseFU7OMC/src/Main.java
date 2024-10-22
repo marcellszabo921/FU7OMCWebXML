0b64cc59bb0a40c4f34867b302ce9f3a8df627be
marcellszabo921 <marcellszabo921@gmail.com>
2024-10-22 10:58:02 +0200

    DomParse

--- FU7OMC_1015/DomParseFU7OMC/DomParseFU7OMC/src/Main.java
+++ FU7OMC_1015/DomParseFU7OMC/DomParseFU7OMC/src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+import domfu7omc1015.DOMRead;
+import domfu7omc1015.DomWriteFU7OMC;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String path = "src/resources/FU7OMC_orarend.xml";
+
+        DOMRead dm = new DOMRead(path);
+        Document dom = dm.buildUpDom();
+        DomWriteFU7OMC write = new DomWriteFU7OMC(dom.getDocumentElement());
+
+        try {
+            write.printXmlStructure(dom.getDocumentElement(),"");
+            write.writeXmlFile(dom, "src/resources/FU7OMC_orarend1.xml");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
