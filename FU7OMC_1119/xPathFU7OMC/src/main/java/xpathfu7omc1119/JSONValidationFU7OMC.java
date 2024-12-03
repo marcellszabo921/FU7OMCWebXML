fad46664a1c79f4160bfd38f232df6ccca0b77af
szabomarcell <112873475+marcellszabo921@users.noreply.github.com>
2024-12-03 10:05:30 +0100

    xmltask

--- FU7OMC_1119/xPathFU7OMC/src/main/java/xpathfu7omc1119/JSONValidationFU7OMC.java
+++ FU7OMC_1119/xPathFU7OMC/src/main/java/xpathfu7omc1119/JSONValidationFU7OMC.java
@@ -1,2 +1,36 @@
-package xpathfu7omc1119;public class JSONValidationFU7OMC {
+package xpathfu7omc1119;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class JSONValidationFU7OMC {
+
+    public static void main(String[] args) {
+        String schemaPath = "src/main/resources/orarendFU7OMCSchema.json";
+        String jsonPath = "src/main/resources/orarendFU7OMC.json";
+        {
+
+            try(InputStream schemaStream = new FileInputStream(schemaPath);
+                InputStream jsonStream = new FileInputStream(jsonPath)) {
+
+                JSONObject schemaJson = new JSONObject(new JSONTokener(schemaStream));
+                Schema schema = SchemaLoader.load(schemaJson);
+
+                JSONObject json = new JSONObject(new JSONTokener(jsonStream));
+
+                schema.validate(json);
+                System.out.println("A JSON érvényes a séma alapján");
+
+            }catch(Exception e) {
+                System.out.println("Hiba történt a validáció során.");
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
 }
