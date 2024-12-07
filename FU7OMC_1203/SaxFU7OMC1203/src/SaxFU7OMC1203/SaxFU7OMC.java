10f8b4ce9d354fdf74f43562374a09f5dc8d5aaf
szabomarcell <112873475+marcellszabo921@users.noreply.github.com>
2024-12-07 19:12:05 +0100

    1203

--- FU7OMC_1203/SaxFU7OMC1203/src/SaxFU7OMC1203/SaxFU7OMC.java
+++ FU7OMC_1203/SaxFU7OMC1203/src/SaxFU7OMC1203/SaxFU7OMC.java
@@ -1,2 +1,23 @@
-package SaxFU7OMC1203;public class SaxFU7OMC {
+package SaxFU7OMC1203;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+
+public class SaxFU7OMC {
+
+    public static void main(String[] args) {
+        try {
+            //Dokumentum olvasó létrehozása, SAXParseFactor osztály newInstance metódus
+            SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+
+            //Saját eseménykezelő létrehozása
+            SaxHandler handler = new SaxHandler();
+
+            //Dokumentum értelmezési folyamatának elindítása
+            parser.parse(new File("src/resources/FU7OMC_kurzusfelvetel.xml"), handler);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
